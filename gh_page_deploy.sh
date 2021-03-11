1b673a43fa31426d2ec28fc9105d0405566cc6ac
littlechin <littlechintw@gmail.com>
2021-03-11 09:35:20 +0800

    Fix to history mode

--- gh_page_deploy.sh
+++ gh_page_deploy.sh
@@ -1,9 +1,8 @@
 npm run build
 cd dist
-git init
 echo "ntpu-demo1.littlechin.tw" > CHAME
-git add .
+cd ..
+git add dist -f
 git commit -m 'Update the gh_page'
-# git push origin :gh-pages
-# git subtree push --prefix dist origin gh-pages
-git push -f git@github.com:littlechintw/NTPUSU-21-Survey.git master:gh-pages
+git push origin :gh-pages
+git subtree push --prefix dist origin gh-pages
